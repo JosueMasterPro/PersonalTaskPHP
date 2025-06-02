bbf86cf310432e4a55e6252c19d3ef4451a05d8e
JosueMasterPro <68764447+JosueMasterPro@users.noreply.github.com>
2025-06-01 18:40:26 -0600

    Cambios en el index y agregamos Database.php

---
+++ src/Database.php
@@ -0,0 +1,23 @@
+<?php
+class Database {
+    private $host = "TU_HOST";
+    private $db_name = "TU_DB";
+    private $username = "TU_USER";
+    private $password = "TU_PASS";
+    public $conn;
+
+    public function getConnection() {
+        $this->conn = null;
+        try {
+            $this->conn = new PDO(
+                "mysql:host=" . $this->host . ";dbname=" . $this->db_name,
+                $this->username,
+                $this->password
+            );
+            $this->conn->exec("set names utf8");
+        } catch(PDOException $exception) {
+            echo "Error de conexión: " . $exception->getMessage();
+        }
+        return $this->conn;
+    }
+}
