b95cc79566c5a268ab4cc4078ef3cfee0927507c
JosueMasterPro <68764447+JosueMasterPro@users.noreply.github.com>
2025-06-01 19:54:06 -0600

    permitiendo solicitudes desde localhost:3000

--- app/index.php
+++ app/index.php
@@ -8,6 +8,16 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
 
+$app->add(function ($request, $handler) {
+    $response = $handler->handle($request);
+    return $response
+        ->withHeader('Access-Control-Allow-Origin', 'http://localhost:3000') // o 'http://localhost:3000' para más seguridad
+        ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+        ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+});
+
+
+
 $app->get('/', function ($request, $response, $args) {
     $response->getBody()->write("¡Hola desde Slim en railway!..");
     return $response;
@@ -44,5 +54,14 @@ $app->get('/api/tareas', function (Request $request, Response $response, $args) 
     return $response->withHeader('Content-Type', 'application/json');
 });
 /* Fin Solicitar las tareas de la BD */
+
+$app->add(function ($request, $handler) {
+    $method = $request->getMethod();
+    $uri = $request->getUri();
+    error_log("Solicitud: {$method} {$uri}");
+
+    return $handler->handle($request);
+});
+
 
 $app->run();
