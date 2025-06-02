1ee75cdc83b976102ca92444403fa04e3e60e47d
JosueMasterPro <68764447+JosueMasterPro@users.noreply.github.com>
2025-06-01 18:03:11 -0600

    Cambiando nombre de la carpeta Public por public

--- Public/index.php
+++ Public/index.php
@@ -4,7 +4,7 @@ require __DIR__ . '/../vendor/autoload.php';
 $app = Slim\Factory\AppFactory::create();
 
 $app->get('/', function ($request, $response, $args) {
-    $response->getBody()->write("¡Hola desde Slim!");
+    $response->getBody()->write("¡Hola desde Slim en railway!");
     return $response;
 });
 
