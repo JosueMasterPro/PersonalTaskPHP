cc0ba191ee5258835834a16bc7c08289b984113e
JosueMasterPro <68764447+JosueMasterPro@users.noreply.github.com>
2025-06-07 20:20:06 -0600

    Añadiendo URL en router.php

--- app/router.php
+++ app/router.php
@@ -1,6 +1,6 @@
 <?php
 // public/router.php
-
+//Ruta api https://personaltaskphp.up.railway.app
 if (php_sapi_name() == 'cli-server') {
     $url  = parse_url($_SERVER['REQUEST_URI']);
     $file = __DIR__ . $url['path'];
